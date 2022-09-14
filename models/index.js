acebd9e5beaa914fb816a617ff3aacb088e58c83
Wild Rivers <jessicamichele11@gmail.com>
2022-09-13 20:50:53 -0600

    added columns

--- models/index.js
+++ models/index.js
@@ -4,13 +4,27 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+// products belong to category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
-// Categories have many Products
+// categories contain many products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
-// Products belongToMany Tags (through ProductTag)
+// products belong to tags through ProductTTag
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
-// Tags belongToMany Products (through ProductTag)
+// tags belong to products through ProductTag
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
